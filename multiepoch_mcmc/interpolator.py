8ad4638487c48c32c49812da64e0ee2c60b35d82
Zac Johnston <zacpjohnston@gmail.com>
2022-06-17 17:53:42 -0400

    [interpolator.py] updated docstrings

--- multiepoch_mcmc/interpolator.py
+++ multiepoch_mcmc/interpolator.py
@@ -5,39 +5,76 @@ from scipy.interpolate import LinearNDInterpolator
 import time
 
 
-class Interpolator:
-    """Interpolates burst properties from model grid
+class GridInterpolator:
+    """
+    A class to quickly interpolate burst variables from a model grid
 
-    parameters
+    Attributes
     ----------
-    grid_file : str
-        filename of model table in `data/model_grid/`
+    file : str
+        filename of model grid located in 'data/model_grid/'
+    grid : pd.DataFrame
+        table of model grid data
     params : [str]
+        list of model parameters
     bvars : [str]
+        list of burst variables
+
+    Methods
+    -------
+    interpolate(x)
+        Interpolates burst variables at given grid coordinate
     """
 
     def __init__(self,
-                 grid_file='johnston_2020.txt',
+                 file='johnston_2020.txt',
                  params=('mdot', 'x', 'z', 'qb', 'g'),
                  bvars=('rate', 'u_rate', 'energy', 'u_energy', 'peak', 'u_peak'),
                  ):
+        """
+        Parameters
+        ----------
+        file : str
+            filename of model table in `data/model_grid/`
+        params : [str]
+            list of input model parameters
+        bvars : [str]
+            list of output burst variables
+        """
         path = os.path.dirname(__file__)
-        filepath = os.path.join(path, '..', 'data', 'model_grid', grid_file)
-        self.filepath = os.path.abspath(filepath)
+        filepath = os.path.join(path, '..', 'data', 'model_grid', file)
+
+        self.file = file
+        self._filepath = os.path.abspath(filepath)
 
         self.params = params
         self.bvars = bvars
         self.grid = None
         self._interpolator = None
 
-        self.load_grid()
-        self.setup_interpolator()
+        self._load_grid()
+        self._setup_interpolator()
 
-    def load_grid(self):
-        """Loads model grid table
+    def interpolate(self, x):
+        """Interpolates burst variables from grid
+
+        Returns: [flt]
+            Interpolated burst variables at point 'x'
+            Matches length and ordering of 'bvars'
+
+        Parameters
+        ----------
+        x : [flt]
+            Coordinates of grid point to interpolate
+            Must exactly match length and ordering of 'params'
         """
-        print(f'Loading grid: {self.filepath}\n')
-        self.grid = pd.read_csv(self.filepath, delim_whitespace=True)
+        return self._interpolator(x)
+
+    def _load_grid(self):
+        """Loads model grid table from file
+        """
+        print(f'Loading grid: {self._filepath}\n')
+        self.grid = pd.read_csv(self._filepath, delim_whitespace=True)
 
         print('Grid parameters\n' + 15*'-')
         for p in self.params:
@@ -45,8 +82,8 @@ class Interpolator:
 
         print(f'\nTotal models: {len(self.grid)}')
 
-    def setup_interpolator(self):
-        """Creates interpolator function from grid
+    def _setup_interpolator(self):
+        """Generates interpolator object from grid
         """
         print('\nGenerating interpolator\n' + 23*'-')
         t0 = time.time()
@@ -68,18 +105,3 @@ class Interpolator:
 
         t1 = time.time()
         print(f'Setup time: {t1-t0:.1f} s')
-
-    def interpolate(self, x):
-        """Interpolate burst variables from grid
-
-        Returns: [flt]
-            Interpolated burst variables at point 'x'
-            Matches length and ordering of 'bvars'
-
-        parameters
-        ----------
-        x : [flt]
-            Coordinates of point to interpolate
-            Must exactly match length and ordering of 'params'
-        """
-        return self._interpolator(x)
