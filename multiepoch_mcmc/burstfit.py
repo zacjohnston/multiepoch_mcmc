2d4aa28c8816c49f5bd38ef9fecc96022ddda842
Zac Johnston <zacpjohnston@gmail.com>
2022-06-21 13:54:37 -0400

    [burstfit.py] more gutting

--- multiepoch_mcmc/burstfit.py
+++ multiepoch_mcmc/burstfit.py
@@ -1,29 +1,39 @@
 import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import astropy.units as u
 import astropy.constants as const
 
 # pyburst
-from multiepoch_mcmc import gravity
-
-c = const.c.to(u.cm / u.s)
-msunyr_to_gsec = (u.M_sun / u.year).to(u.g / u.s)
-mdot_edd = 1.75e-8 * msunyr_to_gsec
-z_sun = 0.01
+from multiepoch_mcmc import accretion, gravity
 
 
 class ZeroLhood(Exception):
+    """Escape redundant calculation if likelihood is zero
+    """
     pass
 
 
 class BurstFit:
     """Class for comparing modelled bursts to observed bursts
     """
+    c = const.c.to(u.cm / u.s)                             # speed of light
+    mdot_edd = 1.75e-8 * (u.M_sun / u.year).to(u.g / u.s)  # eddington accretion rate
+    z_sun = 0.01                                           # solar CNO metallicity
 
     def __init__(self,
-                 source,
+                 grid_interpolator,
+                 priors,
+                 grid_bounds,
+                 weights,
+                 bprops=('rate', 'fluence', 'peak', 'fper', 'fedd'),
+                 analytic_bprops=('fper', 'fedd'),
+                 interp_bprops=('rate', 'fluence', 'peak'),
+                 interp_keys=('mdot', 'x', 'z', 'qb', 'mass'),
+                 param_keys=('mdot1', 'mdot2', 'mdot3', 'x', 'z', 'qb1', 'qb2', 'qb3', 'm_nw', 'm_gr', 'd_b', 'xi_ratio'),
+                 epoch_unique=('mdot', 'qb'),
+                 system='gs1826',
+                 epochs=(1998, 2000, 2007),
                  u_fper_frac=0.0,
                  u_fedd_frac=0.0,
                  zero_lhood=-np.inf,
@@ -32,7 +42,20 @@ class BurstFit:
         reference_radius : float
             Newtonian get_radius (km) used in Kepler
         """
-        self.source = source
+        self.system = system
+        self.epochs = epochs
+        self.n_epochs = len(self.obs_data)
+
+        # temp attributes
+        self.param_keys = param_keys
+        self.interp_keys = interp_keys
+        self.analytic_bprops = analytic_bprops
+        self.bprops = bprops
+        self.interp_bprops = interp_bprops
+        self.param_aliases = {'mass': 'm_nw'}
+        self.epoch_unique = epoch_unique
+        self.grid_bounds = grid_bounds
+        self.weights = weights
 
         self.param_idxs = {}
         self.interp_idxs = {}
@@ -40,22 +63,21 @@ class BurstFit:
 
         self.reference_radius = reference_radius
 
-        self.n_bprops = len(self.mcmc_version.bprops)
-        self.n_analytic_bprops = len(self.mcmc_version.analytic_bprops)
-        self.n_interp_params = len(self.mcmc_version.interp_keys)
+        self.n_bprops = len(self.bprops)
+        self.n_analytic_bprops = len(self.analytic_bprops)
+        self.n_interp_params = len(self.interp_keys)
 
         self.kpc_to_cm = u.kpc.to(u.cm)
         self.zero_lhood = zero_lhood
         self.u_fper_frac = u_fper_frac
         self.u_fedd_frac = u_fedd_frac
 
-        self.kemulator = interpolator.Kemulator()
-        self.obs = None
-        self.n_epochs = None
+        self.interpolator = grid_interpolator
+        self.obs_table = None
         self.obs_data = None
         self.extract_obs_values()
 
-        self.priors = self.mcmc_version.priors
+        self.priors = priors
 
     def get_indexes(self):
         """Extracts indexes of parameters and burst properties
@@ -68,30 +90,27 @@ class BurstFit:
                 dict_out[key] = i
             return dict_out
 
-        self.param_idxs = idx_dict(self.mcmc_version.param_keys)
-        self.interp_idxs = idx_dict(self.mcmc_version.interp_keys)
+        self.param_idxs = idx_dict(self.param_keys)
+        self.interp_idxs = idx_dict(self.interp_keys)
 
     def extract_obs_values(self):
         """Unpacks observed burst properties (dt, fper, etc.) from data
         """
-        filename = f'{self.source_obs}.dat'
-        filepath = os.path.join(PYBURST_PATH, 'files', 'obs_data',
-                                self.source_obs, filename)
+        path = os.path.dirname(__file__)
 
-        self.obs = pd.read_csv(filepath, delim_whitespace=True)
-        self.obs.set_index('epoch', inplace=True, verify_integrity=True)
+        filename = f'{self.system}.dat'
+        filepath = os.path.join(path, '..', 'data', self.system, filename)
 
-        # Select single epoch (if applicable)
-        if self.mcmc_version.epoch is not None:
-            # TODO: define/specify epochs for all mcmc versions?
-            try:
-                self.obs = self.obs.loc[[self.mcmc_version.epoch]]
-            except KeyError:
-                raise KeyError(f'epoch [{self.mcmc_version.epoch}] '
-                               f'not in obs_data table')
+        print(f'Loading obs table: {os.path.abspath(filepath)}')
+        self.obs_table = pd.read_csv(filepath, delim_whitespace=True)
+        self.obs_table.set_index('epoch', inplace=True, verify_integrity=True)
 
-        self.n_epochs = len(self.obs)
-        self.obs_data = self.obs.to_dict(orient='list')
+        try:
+            self.obs_table = self.obs_table.loc[[self.epochs]]
+        except KeyError:
+            raise KeyError(f'epoch(s) not found in obs_data table')
+
+        self.obs_data = self.obs_data.to_dict(orient='list')
 
         for key, item in self.obs_data.items():
             self.obs_data[key] = np.array(item)
@@ -103,15 +122,13 @@ class BurstFit:
         self.obs_data['fper'] *= self.obs_data['cbol']
         self.obs_data['u_fper'] = self.obs_data['fper'] * u_fper_frac
 
-    def lhood(self, x, plot=False):
+    def lhood(self, x):
         """Return lhood for given params
 
         Parameters
         ----------
         x : 1D array
             set of parameter values to try (must match order of mcmc_version.param_keys)
-        plot : bool
-            whether to plot the comparison
         """
         params = self.get_params_dict(x=x)
 
@@ -165,7 +182,7 @@ class BurstFit:
     def get_params_dict(self, x):
         """Returns params in form of dict
         """
-        keys = self.mcmc_version.param_keys
+        keys = self.param_keys
         params_dict = dict.fromkeys(keys)
 
         for i, key in enumerate(keys):
@@ -191,11 +208,7 @@ class BurstFit:
                 Note: Actually the luminosity at this stage, as this is the local value
             """
             out = np.full([self.n_epochs, 2], np.nan, dtype=float)
-
-            if self.mcmc_version.x_edd_option == 'x_0':
-                x_edd = params['x']
-            else:
-                x_edd = self.mcmc_version.x_edd_option
+            x_edd = params['x']
 
             l_edd = accretion.eddington_lum_newtonian(mass=params['m_nw'], x=x_edd)
             out[:, 0] = l_edd
@@ -209,9 +222,9 @@ class BurstFit:
             out = np.full([self.n_epochs, 2], np.nan, dtype=float)
             mass_ratio, redshift = self.get_gr_factors(params=params)
 
-            phi = (redshift - 1) * c.value ** 2 / redshift  # gravitational potential
+            phi = (redshift - 1) * self.c.value ** 2 / redshift  # gravitational potential
             mdot = epoch_params[:, self.interp_idxs['mdot']]
-            l_per = mdot * mdot_edd * phi
+            l_per = mdot * self.mdot_edd * phi
 
             out[:, 0] = l_per
             out[:, 1] = out[:, 0] * self.u_fper_frac
@@ -220,7 +233,7 @@ class BurstFit:
         function_map = {'fper': get_fper, 'fedd': get_fedd}
         analytic = np.full([self.n_epochs, 2*self.n_analytic_bprops], np.nan, dtype=float)
 
-        for i, bprop in enumerate(self.mcmc_version.analytic_bprops):
+        for i, bprop in enumerate(self.analytic_bprops):
             analytic[:, 2*i: 2*(i+1)] = function_map[bprop]()
 
         return analytic
@@ -233,7 +246,7 @@ class BurstFit:
 
         # ==== shift interpolated bprops ====
         # TODO: concatenate bprop arrays and handle together
-        for i, bprop in enumerate(self.mcmc_version.interp_bprops):
+        for i, bprop in enumerate(self.interp_bprops):
             i0 = 2 * i
             i1 = 2 * (i + 1)
             interp_shifted[:, i0:i1] = self.shift_to_observer(
@@ -241,7 +254,7 @@ class BurstFit:
                                                     bprop=bprop, params=params)
 
         # ==== shift analytic bprops ====
-        for i, bprop in enumerate(self.mcmc_version.analytic_bprops):
+        for i, bprop in enumerate(self.analytic_bprops):
             i0 = 2 * i
             i1 = 2 * (i + 1)
             analytic_shifted[:, i0:i1] = self.shift_to_observer(
@@ -255,14 +268,14 @@ class BurstFit:
         lh = 0.0
         all_shifted = np.concatenate([interp_shifted, analytic_shifted], axis=1)
 
-        for i, bprop in enumerate(self.mcmc_version.bprops):
+        for i, bprop in enumerate(self.bprops):
             bprop_idx = 2 * i
             u_bprop_idx = bprop_idx + 1
 
             model = all_shifted[:, bprop_idx]
             u_model = all_shifted[:, u_bprop_idx]
 
-            lh += self.compare(model=model, u_model=u_model, bprop=bprop, label=bprop)
+            lh += self.compare(model=model, u_model=u_model, bprop=bprop)
 
         return lh
 
@@ -325,7 +338,7 @@ class BurstFit:
         interp_params : 1darray
             parameters specific to the model (e.g. mdot1, x, z, qb, get_mass)
         """
-        output = self.kemulator.emulate_burst(params=interp_params)
+        output = self.interpolator.interpolate(params=interp_params)
 
         if True in np.isnan(output):
             raise ZeroLhood
@@ -338,7 +351,7 @@ class BurstFit:
         epoch_params = np.full((self.n_epochs, self.n_interp_params), np.nan, dtype=float)
 
         for i in range(self.n_epochs):
-            for j, key in enumerate(self.mcmc_version.interp_keys):
+            for j, key in enumerate(self.interp_keys):
                 epoch_params[i, j] = self.get_interp_param(key, params, epoch_idx=i)
 
         return epoch_params
@@ -346,9 +359,9 @@ class BurstFit:
     def get_interp_param(self, key, params, epoch_idx):
         """Extracts interp param value from full params
         """
-        key = self.mcmc_version.param_aliases.get(key, key)
+        key = self.param_aliases.get(key, key)
 
-        if key in self.mcmc_version.epoch_unique:
+        if key in self.epoch_unique:
             key = f'{key}{epoch_idx + 1}'
 
         return params[key]
@@ -364,11 +377,11 @@ class BurstFit:
     def lnprior(self, x, params):
         """Return logarithm prior lhood of params
         """
-        lower_bounds = self.mcmc_version.grid_bounds[:, 0]
-        upper_bounds = self.mcmc_version.grid_bounds[:, 1]
+        lower_bounds = self.grid_bounds[:, 0]
+        upper_bounds = self.grid_bounds[:, 1]
+
         inside_bounds = np.logical_and(x > lower_bounds,
                                        x < upper_bounds)
-
         if False in inside_bounds:
             raise ZeroLhood
 
@@ -378,7 +391,7 @@ class BurstFit:
 
         return prior_lhood
 
-    def compare(self, model, u_model, bprop, label='', plot=False):
+    def compare(self, model, u_model, bprop):
         """Returns logarithmic likelihood of given model values
 
         Calculates difference between modelled and observed values.
@@ -392,19 +405,13 @@ class BurstFit:
             Corresponding model uncertainties
         bprop : str
             burst property being compared
-        label : str
-            label of parameter to print
-        plot : bool
-            whether to plot the comparison
         """
         obs = self.obs_data[bprop]
         u_obs = self.obs_data[f'u_{bprop}']
 
-        weight = self.mcmc_version.weights[bprop]
+        weight = self.weights[bprop]
         inv_sigma2 = 1 / (u_model ** 2 + u_obs ** 2)
         lh = -0.5 * weight * ((model - obs) ** 2 * inv_sigma2
                               + np.log(2 * np.pi / inv_sigma2))
 
-        if plot:
-            self.plot_compare(model=model, u_model=u_model, bprop=label)
         return lh.sum()
