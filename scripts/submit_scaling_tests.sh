47a8027e60e703d807b68904c8263c6cf8ae2c42
Zac Johnston <zacpjohnston@gmail.com>
2022-06-29 14:39:10 -0400

    [scripts/submit_scaling_tests.sb] created script

---
+++ scripts/submit_scaling_tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $# -ne 3 ]]; then
+  echo -e "\nusage: `basename $0` <N_STEPS> <N_WALKERS> <MAX_THREADS>\n"
+  exit 0
+fi
+
+N_STEPS=${1}
+N_WALKERS=${2}
+MAX_THREADS=${3}
+
+for i in {0..7}; do
+	(( N_THREADS=2**i ))
+
+  sbatch --job-name="${MODEL}" \
+    --ntasks="${N_THREADS}" \
+    --export=n_steps="${N_STEPS}",n_walkers="${N_WALKERS}" \
+    --job-name="${N_WALKERS}_${N_THREADS}" \
+    submit_job.sb
+
+	if [ "${N_THREADS}" -eq "${MAX_THREADS}" ]; then
+		break
+	fi
+done
+
