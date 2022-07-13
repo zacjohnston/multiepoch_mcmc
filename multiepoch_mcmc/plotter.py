8f8b933df7b8b0872a728931025fb2fb4edf1d42
Zac Johnston <zacpjohnston@gmail.com>
2022-07-13 16:42:10 -0400

    [plotter.py] added get_autocorr_time

--- multiepoch_mcmc/plotter.py
+++ multiepoch_mcmc/plotter.py
@@ -28,11 +28,23 @@ class MCPlotter:
         self.discard = discard
 
         self._config = config.load_config(system)
-        self.backend = mcmc.open_backend(system=system, n_walkers=n_walkers)
-
-        self.n_steps = self.backend.iteration
-        self.chain = self.backend.get_chain(discard=discard)
-
         self.params = self._config['keys']['params']
         self.n_dim = len(self.params)
-        self.accept_frac = self.backend.accepted.mean() / self.n_steps
+
+        self._backend = mcmc.open_backend(system=system, n_walkers=n_walkers)
+
+        self.n_steps = self._backend.iteration
+        self.chain = self._backend.get_chain(discard=discard)
+        self.filename = self._backend.filename
+        self.lhood = self._backend.get_log_prob()
+
+        self.accept_frac = self._backend.accepted.mean() / self.n_steps
+
+        self.tau = None
+        self.get_autocorr_time()
+
+    def get_autocorr_time(self):
+        """Estimate autocorrelation time
+        """
+        print('Calculating autocorrelation time')
+        self.tau = self._backend.get_autocorr_time(discard=self.discard, tol=0)
