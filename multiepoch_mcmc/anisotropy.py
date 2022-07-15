634f05191506dd3fbce154f70b331e314f3eaaff
Zac Johnston <zacpjohnston@gmail.com>
2022-07-15 13:52:47 -0400

    [anisotropy.py] unpack inverse values

--- multiepoch_mcmc/anisotropy.py
+++ multiepoch_mcmc/anisotropy.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from scipy.interpolate import interp1d
 
 
 class DiskModel:
@@ -20,19 +21,30 @@ class DiskModel:
         self.table = load_table(model)
 
 
-def load_table(model):
+def load_table(model,
+               components=('p', 'b', 'd', 'r'),
+               ):
     """Loads anisotropy table from file
 
     Parameters
     ----------
     model : str
         name of disk anisotropy model
+    components : [str]
+        list of anisotropy components (by suffix letter)
     """
     path = os.path.dirname(__file__)
     filename = f'{model}.csv'
     filepath = os.path.join(path, '..', 'data', 'anisotropy', filename)
 
-    print(f'Loading anisotropy table: {os.path.abspath(filepath)}')
-    table = pd.read_csv(filepath)
+    try:
+        print(f'Loading anisotropy table: {os.path.abspath(filepath)}')
+        table = pd.read_csv(filepath)
+    except FileNotFoundError:
+        raise FileNotFoundError(f'No table exists for anisotropy model `{model}`')
+
+    # unpack inverse values
+    for c in components:
+        table[f'xi_{c}'] = 1 / table[f'1/xi_{c}']
 
     return table
