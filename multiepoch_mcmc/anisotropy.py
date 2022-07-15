9b85c8e6d34b3564122abdf2441f2731d110bb43
Zac Johnston <zacpjohnston@gmail.com>
2022-07-15 13:36:42 -0400

    [anisotropy.py] created DiskModel class

--- multiepoch_mcmc/anisotropy.py
+++ multiepoch_mcmc/anisotropy.py
@@ -2,13 +2,31 @@ import os
 import pandas as pd
 
 
+class DiskModel:
+    """
+    Class for disk anisotropy model
+    """
+
+    def __init__(self,
+                 model='he16_a',
+                 ):
+        """
+        Parameters
+        ----------
+        model : str
+            name of anisotropy disk model
+        """
+        self.model = model
+        self.table = load_table(model)
+
+
 def load_table(model):
     """Loads anisotropy table from file
 
     Parameters
     ----------
     model : str
-        name of anisotropy model
+        name of disk anisotropy model
     """
     path = os.path.dirname(__file__)
     filename = f'{model}.csv'
